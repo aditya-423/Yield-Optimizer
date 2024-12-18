5852a5308c677b6325d277e21530d717724538cb
aditya-423 <163311626+aditya-423@users.noreply.github.com>
2024-12-19 02:32:35 +0530

    Update fluid.js
    Added comments and constants

--- fluid.js
+++ fluid.js
@@ -10,6 +10,8 @@ const tokenAddress = "0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9"; // Replace wi
 
 const contract = new ethers.Contract(contractAddress, pool_abi, provider);
 
+const USDT_DECIMALS = 6;
+
 // Step 3: Write the async function to call getOverallTokenData
 (async () => {
 
@@ -27,16 +29,18 @@ const contract = new ethers.Contract(contractAddress, pool_abi, provider);
       rateData
     } = data;
 
-    const totSupply = parseFloat(ethers.utils.formatUnits(totalSupply, 6));
-    const totBorrow = parseFloat(ethers.utils.formatUnits(totalBorrow, 6));
+    const totSupply = parseFloat(ethers.utils.formatUnits(totalSupply, USDT_DECIMALS)); // Total Amount of USDT Borrowed on Fluid
+    const totBorrow = parseFloat(ethers.utils.formatUnits(totalBorrow, USDT_DECIMALS)); // Total Amount of USDT Supplied on Fluid
     const rateDataV2 = rateData.rateDataV2;
-    const k1 = ethers.utils.formatUnits(rateDataV2.kink1, 2)/100;
-    const k2 = ethers.utils.formatUnits(rateDataV2.kink2, 2)/100;
-    const rateAtUtilizationZero = ethers.utils.formatUnits(rateDataV2.rateAtUtilizationZero, 2)/100;
-    const rateAtUtilizationKink1 = ethers.utils.formatUnits(rateDataV2.rateAtUtilizationKink1, 2)/100;
-    const rateAtUtilizationKink2 = ethers.utils.formatUnits(rateDataV2.rateAtUtilizationKink2, 2)/100;
-    const rateAtUtilizationMax = ethers.utils.formatUnits(rateDataV2.rateAtUtilizationMax, 2)/100;
-    const rf = 0.1;
+    const k1 = ethers.utils.formatUnits(rateDataV2.kink1, 2)/100; // Kink 1 for Fluid
+    const k2 = ethers.utils.formatUnits(rateDataV2.kink2, 2)/100; // Kink 2 for Fluid
+    const rateAtUtilizationZero = ethers.utils.formatUnits(rateDataV2.rateAtUtilizationZero, 2)/100; // Borrow Rate at Utilization Zero
+    const rateAtUtilizationKink1 = ethers.utils.formatUnits(rateDataV2.rateAtUtilizationKink1, 2)/100; // Borrow Rate at Utilization Kink 1
+    const rateAtUtilizationKink2 = ethers.utils.formatUnits(rateDataV2.rateAtUtilizationKink2, 2)/100; // Borrow Rate at Utilization Kink 2
+    const rateAtUtilizationMax = ethers.utils.formatUnits(rateDataV2.rateAtUtilizationMax, 2)/100; // Borrow Rate at Max Utilization
+    const rf = 0.1; // Reserve Factor for Fluid (assumed static)
+    
+    // Output all variables as a JSON object
     console.log(JSON.stringify({ k1, k2, rf, totSupply, totBorrow, rateAtUtilizationZero, rateAtUtilizationKink1, rateAtUtilizationKink2, rateAtUtilizationMax }));
     
 })();
